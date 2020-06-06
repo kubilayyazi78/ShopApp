9cfc94f2261c9e8ecf595beae80c866d3e7cff85
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-06-07 02:26:43 +0300

    Repository pattern uygulanmaya başlandı -1.

---
+++ ShopApp.DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,21 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShopApp.DataAccess.Abstract
+{
+   public interface ICategoryDal
+    {
+
+        Category GetById(int id);
+        Category GetOne(Expression<Func<Category, bool>> filter);
+        IQueryable<Category> GetAll(Expression<Func<Category, bool>> filter);
+        void Create(Category entity);
+        void Delete(Category entity);
+        void Update(Category entity);
+
+    }
+}
