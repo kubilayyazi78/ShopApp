951a325038dc835301f7e5d9eae0c60b552d053f
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-06-07 03:32:30 +0300

    Generic repository tamamlandı.

--- ShopApp.DataAccess/Concrete/EfCore/EfCoreProductDal.cs
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreProductDal.cs
@@ -8,44 +8,9 @@ using System.Text;
 
 namespace ShopApp.DataAccess.Concrete.EfCore
 {
-    public class EfCoreProductDal : IProductDal
+    public class EfCoreProductDal : EfCoreGenericRepository<Product, ShopContext>, IProductDal
     {
-
-
-        ShopContext db = new ShopContext();
-
-        public void Create(Product entity)
-        {
-            db.Products.Add(entity);
-            db.SaveChanges();
-        }
-
-        public void Delete(Product entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryable<Product> GetAll(Expression<Func<Product, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Product GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Product GetOne(Expression<Func<Product, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
         public IEnumerable<Product> GetPopularProducts()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Product entity)
         {
             throw new NotImplementedException();
         }
