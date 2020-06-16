671049407c8e23ec1534818f5ac41b6d72bace83
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-06-16 23:37:03 +0300

    Productmodele validationlar eklendi.

--- ShopApp.WebUI/Models/ProductModel.cs
+++ ShopApp.WebUI/Models/ProductModel.cs
@@ -1,6 +1,7 @@
 ﻿using ShopApp.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,12 +11,18 @@ namespace ShopApp.WebUI.Models
     {
         public int Id { get; set; }
 
+
+        [Required]
+        [StringLength(60,MinimumLength =10,ErrorMessage ="Ürün ismi minimum 10 ve maksimum 60 karakter olmalıdır.")]
         public string Name { get; set; }
-
+        [Required]
         public string ImageUrl { get; set; }
-
+        [Required]
+        [StringLength(100, MinimumLength = 20, ErrorMessage = "Ürün açıklaması minimum 20 ve maksimum 100 karakter olmalıdır.")]
         public string Description { get; set; }
-        public decimal Price { get; set; }
+        [Required(ErrorMessage ="Fiyat Belirtiniz.")]
+        [Range(1,10000)]
+        public decimal? Price { get; set; }
 
         public List<Category>  SelectedCategories { get; set; }
     }
