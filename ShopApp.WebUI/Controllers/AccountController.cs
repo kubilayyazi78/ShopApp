d6a30b19c7d49312cd1892a86fadbfce8ac606d6
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-06-19 21:07:45 +0300

    Kullanıcı login işlemi yapıldı.

--- ShopApp.WebUI/Controllers/AccountController.cs
+++ ShopApp.WebUI/Controllers/AccountController.cs
@@ -55,5 +55,39 @@ namespace ShopApp.WebUI.Controllers
             ModelState.AddModelError("", "Bilinmeyen hata olustu");
             return View(model);
         }
+
+
+        public IActionResult Login()
+        {
+            return View(new LoginModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model, string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? "~/";
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByNameAsync(model.Username);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Bu kullanıcı ile daha önce hesap oluşturulmamış.");
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, true, false);
+
+            if (result.Succeeded)
+            {
+                return Redirect(returnUrl);
+            }
+
+            ModelState.AddModelError("", "Kullanıcı adı ve ya parola yanlış");
+            return View(model);
+        }
     }
 }
