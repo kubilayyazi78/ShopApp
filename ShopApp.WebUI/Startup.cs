bbc80b5a49fe74c1d0728aea01af99a6e26a9d46
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-06-21 17:41:28 +0300

    Admin rolü oluşturuldu.

--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -96,7 +96,7 @@ namespace ShopApp.WebUI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,UserManager<ApplicationUser> userManager , RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
@@ -147,6 +147,9 @@ namespace ShopApp.WebUI
             //{
             //    await context.Response.WriteAsync("Hello World!");
             //});
+
+
+            SeedIdentity.Seed(userManager, roleManager, Configuration).Wait();
         }
     }
 }
