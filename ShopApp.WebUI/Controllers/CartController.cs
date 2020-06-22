8965b2bac501a0639538e3ea2cabd2977df2fd45
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-06-22 23:32:55 +0300

    Cart view oluşturuldu.

--- ShopApp.WebUI/Controllers/CartController.cs
+++ ShopApp.WebUI/Controllers/CartController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ShopApp.Business.Abstract;
 using ShopApp.WebUI.Identity;
+using ShopApp.WebUI.Models;
 
 namespace ShopApp.WebUI.Controllers
 {
@@ -21,11 +22,23 @@ namespace ShopApp.WebUI.Controllers
             _cartService = cartService;
             _userManager = userManager;
         }
-      
+
         public IActionResult Index()
         {
             var cart = _cartService.GetCartByUserId(_userManager.GetUserId(User));
-            return View();
+            return View(new CartModel()
+            {
+                CartId = cart.Id,
+                CartItems = cart.CartItems.Select(i => new CartItemModel()
+                {
+                    CartItemId = i.Id,
+                    ProductId = i.Product.Id,
+                    Name = i.Product.Name,
+                    Price = (decimal)i.Product.Price,
+                    ImageUrl = i.Product.ImageUrl,
+                    Quantity = i.Quantity
+                }).ToList()
+            });
         }
 
         [HttpPost]
